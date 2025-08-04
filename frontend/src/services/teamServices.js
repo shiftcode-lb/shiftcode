6d951c78f66e27ddba32b092f21c51ab2b749d69
mohamadkassemm <mohammaddkassemm@gmail.com>
2025-08-04 15:10:03 +0300

    fixed url part 3

--- frontend/src/services/teamServices.js
+++ frontend/src/services/teamServices.js
@@ -3,6 +3,7 @@ import axiosInstance from './axiosInstance'
 export const fetchTeam = async () =>{
     try {
         const res = await axiosInstance.get('/team');
+        console.log("test",res)
         return res.data;
     } catch (error) {
         console.error("Error getting data", error);
