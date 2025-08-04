69e5270f57e4f80509290e55686c2957391acfac
mohamadkassemm <mohammaddkassemm@gmail.com>
2025-08-04 15:29:55 +0300

    debugging2

--- frontend/src/services/axiosInstance.js
+++ frontend/src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: "https://shiftcode.onrender.com/api/v1",
     headers: {
     'Content-Type': 'application/json',
   },
