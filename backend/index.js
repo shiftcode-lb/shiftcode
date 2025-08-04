4177388dc3b98a41f5a0ddeeb6994071d077f1bd
mohamadkassemm <mohammaddkassemm@gmail.com>
2025-08-04 15:42:27 +0300

    debugging url

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const DB = require("./database").connectDB;
-require('dotenv').config();
 
 const domainURL = process.env.DOMAIN_URL
 
