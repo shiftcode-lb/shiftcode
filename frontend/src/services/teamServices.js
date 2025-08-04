4ac7683794716e6fc2c72376cc05495dd78cf516
mohamadkassemm <mohammaddkassemm@gmail.com>
2025-08-04 15:23:06 +0300

    debugging

--- frontend/src/services/teamServices.js
+++ frontend/src/services/teamServices.js
@@ -2,6 +2,7 @@ import axiosInstance from './axiosInstance'
 
 export const fetchTeam = async () =>{
     try {
+        console.log("hi")
         const res = await axiosInstance.get('/team');
         console.log("test",res)
         return res.data;
