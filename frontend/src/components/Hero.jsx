48dfda2b90aa46aca88271839bee3db01730596d
Ali Issa <alyy.issa01@gmail.com>
2025-08-07 19:07:57 +0300

    test1

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
     });
   }, []);
   return (
-    <div className='flex flex-col gap-10 justify-center  md:flex-row items-center h-[90vh] relative
+    <div className='flex flex-col gap-10 justify-center  md:flex-row items-center min-h-[90vh] relative
     w-full mx-auto px-3 sm:px-4 md:px-11 lg:px-13 xl:px-12 2xl:px-16 max-w-screen-xl pt-20'>
       
         {/** Left Side Section */}
