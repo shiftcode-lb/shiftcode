b10080135a8c67318fe96bc16e9db711c3d30974
Ali Issa <alyy.issa01@gmail.com>
2025-08-29 15:45:24 +0300

    test

--- frontend/src/services/projectServices.js
+++ frontend/src/services/projectServices.js
@@ -3,6 +3,7 @@ import axiosInstance from './axiosInstance'
 export const fetchProjects = async () =>{
     try {
         const res = await axiosInstance.get('/projects/count');
+        console.log(res.data)
         return res.data;
     } catch (error) {
         console.error("Error getting data", error);
--- frontend/src/services/teamServices.js
+++ frontend/src/services/teamServices.js
@@ -3,6 +3,7 @@ import axiosInstance from './axiosInstance'
 export const fetchTeam = async () =>{
     try {
         const res = await axiosInstance.get('/team/count');
+        console.log("team" + res.data)
         return res.data;
     } catch (error) {
         console.error("Error getting data", error);
