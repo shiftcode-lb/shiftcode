0efc9c8d4c33276b8dd2c0b8da3cdf1cc3dd9ef3
Ali Issa <alyy.issa01@gmail.com>
2026-01-07 18:36:48 +0200

    Test 1

--- frontend/src/components/Team.jsx
+++ frontend/src/components/Team.jsx
@@ -4,7 +4,50 @@ import TeamCard from './TeamCard';
 import { assets } from '../assets/assests';
 
 const Team = () => {
+  const team = [
+    {
+      name: "Mohammad Kassem",
+      email: "mohammaddkassemm@gmail.com",
+      position: "Co-Founder",
+      image: assets.mhmd,
+      socialLinks: {
+        instagram: "https://www.instagram.com/mohamadkassemm",
+        linkedin: "https://www.linkedin.com/in/mohammad-kassem-57a91524a",
+        github: "https://github.com/mohamadkassemm"
+      }
+    },
 
+    {
+      name: "Ali Issa",
+      email: "alyy.issa01@gmail.com",
+      position: "Co-Founder",
+      image: assets.ali,
+      socialLinks: {
+        instagram: "https://www.instagram.com/alyy.issa",
+        linkedin: "https://www.linkedin.com/in/ali-issa-18034234b",
+        github:"https://github.com/alyyissa"
+        }
+      },
+      {
+        name: "Batoul Issa",
+        email: "batoulissa510@gmail.com",
+        position: "Localization Specialist",
+        image: assets.batol,
+        socialLinks: {
+          linkedin: "https://www.linkedin.com/in/batoul-i-49699b283"
+        }
+      },
+      {
+        name: "Elham Zaiour",
+        email: "elhamzaiour@gmail.com",
+        position: "UI/UX Developer",
+        image: assets.elham,
+        socialLinks:{
+          instagram: "https://www.instagram.com/elham.zaiour",
+          linkedin: "https://www.linkedin.com/in/elham-zaiour-6296b5358/"
+        }
+      }
+  ]
   return (
     <div className='relative flex flex-col justify-center items-center mb-10
       w-full mx-auto px-3 sm:px-4 md:px-11 lg:px-13 xl:px-12 2xl:px-16 max-w-screen-xl'>
@@ -27,10 +70,7 @@ const Team = () => {
             name={teamMember.name}
             email={teamMember.email}
             position={teamMember.position}
-            description={teamMember.description}
             image={teamMember.image}
-            // NOTE: socialLinks already comes from API, 
-            // members[teamMember.name] kept as reference for local assets
             socialLinks={teamMember.socialLinks}
           />
         ))}
